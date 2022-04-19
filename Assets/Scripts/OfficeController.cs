db0d21733730690a6f5a6570d2804232460f22a3
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2022-04-19 10:38:32 -0500

    Added title screen and working doors

---
+++ Assets/Scripts/OfficeController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeController : MonoBehaviour
+{
+    public Animator ventAnimator;
+    public Animator leftDoorAnimator;
+    public Animator rightDoorAnimator;
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        if(mouse.x < 860 && transform.rotation.eulerAngles.y > 70.25f){
+            transform.Rotate(0, -0.25f, 0);
+        } else if(mouse.x > 1060 && transform.rotation.eulerAngles.y < 110f){
+            transform.Rotate(0, 0.25f, 0);
+        }
+    }
+
+    public void ToggleDoor(){
+        if(transform.rotation.eulerAngles.y >= 70f && transform.rotation.eulerAngles.y < 85f){
+            bool isClosed = leftDoorAnimator.GetBool("DoorClosed");
+            leftDoorAnimator.SetBool("DoorClosed", !isClosed);
+        } else if(transform.rotation.eulerAngles.y >= 85f && transform.rotation.eulerAngles.y <= 95f){
+            bool isClosed = ventAnimator.GetBool("VentClosed");
+            ventAnimator.SetBool("VentClosed", !isClosed);
+        } else if(transform.rotation.eulerAngles.y > 95f && transform.rotation.eulerAngles.y <= 111f){
+            bool isClosed = rightDoorAnimator.GetBool("DoorClosed");
+            rightDoorAnimator.SetBool("DoorClosed", !isClosed);
+        }
+    }
+}
